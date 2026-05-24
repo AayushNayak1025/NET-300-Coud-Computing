7c69eee6b4ece68500fcab36535c5299ec3265b2
AayushNayak1025 <aayushnayak002@gmail.com>
2026-05-24 22:20:52 +0545

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,19 @@
+balance = int(input("Enter current balance (NPR): "))
+daily_withdrawn = int(input("Enter amount already withdrawn today (NPR): "))
+amount = int(input("Enter withdrawal amount (NPR): "))
+
+daily_limit = 50000
+
+if amount % 500 != 0:
+    print("Invalid amount. Must be a multiple of NPR 500.")
+
+elif amount > balance:
+    print("Insufficient balance.")
+
+elif (daily_withdrawn + amount) > daily_limit:
+    print("Daily withdrawal limit reached.")
+
+else:
+    balance -= amount
+    print("Withdrawal successful.")
+    print("Your current balance after withdrawal: NPR", balance)
